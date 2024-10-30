76e58d6a36135ac22f266cfa6e43793b6ffff063
Emmanueldesirekouadio <kouadiotiamz@gmail.com>
2024-10-30 16:01:51 +0000

    alterco35522

--- src/app/core/interceptor/jwt.interceptor.ts
+++ src/app/core/interceptor/jwt.interceptor.ts
@@ -5,30 +5,27 @@ import { catchError, Observable, throwError,  } from "rxjs";
 import { LocalstorageService } from "../services/baseService/Localstorage.service";
 
 
-export class  jwtInterceptor implements HttpInterceptor{
-  router: any; 
-  constructor(private localStorage: LocalstorageService){}
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let authReq: any;
-    const token : string | null = this.localStorage.getToken();
-    if(token){
-      authReq = request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`
+export function jwtInterceptor(localStorage: LocalstorageService, router: any): HttpInterceptor {
+    return {
+        intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+            let authReq: any;
+            const token: string | null = localStorage.getToken();
+            if (token) {
+                authReq = request.clone({
+                    setHeaders: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+            }
+            return next.handle(authReq).pipe(
+                catchError((err: HttpErrorResponse) => {
+                    if (err.status === 401 || err.status === 403 || (err.status === 400 && err.error.codeError === "INSUFFICIENT_AUTHENTICATION")) {
+                        localStorage.destroyToken();
+                        router.navigate(["/login"]);
+                    }
+                    return throwError(() => new Error(err.error.message || err.message));
+                })
+            );
         }
-      });
-
-    }
-    return next.handle(authReq).pipe(
-      catchError((err: HttpErrorResponse) => {
-        if(err.status === 401 || err.status === 403 || (err.status === 400 && err.error.codeError === "INSUFFICIENT_AUTHENTICATION")){
-          this.localStorage.destroyToken();
-          this.router.navigate(["/login"]);
-        }
-        return throwError(() => new Error(err.error.message || err.message))
-      })
-    )
-
-
-  }
+    };
 }
